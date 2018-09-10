4d8f5c153044d45b111e1f4d8eba0f802ae8fe94
Joey Bullock <35963215+joeybullock@users.noreply.github.com>
2018-09-10 08:32:52 -0400

    Update API key
    Input of Inspector ID and date range will return inspection info from Accela Construct API, after which the address info will be passed to Google Maps APIs to route the inspections.

--- NodeJS/src/main.js
+++ NodeJS/src/main.js
@@ -4,22 +4,21 @@ var bodyParcer = require("body-parser");
 var https = require("https");
 var app = express();
 
-var accelaOptions = {
-    host: 'apis.accela.com',
-    port: 443,
-    path: 'v4/inspections?module=Building',
-    method: 'GET'
-};
-var accelaHeaders = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'x-accela-appid': '636603438780966771',
-    'Authorization': 'fbD2fS4UE1u2xUc9CjTNRnGthIk6SVvBvJxD7SF8XzgtYRTgzOD3EkLA3ovmFarM9zTXVpX4oalW1L3q6jsE_bTQLNSm5Oa297_seZeFbQnOdVd2sZTLkuIMAfTdQtnStKGtACPJ0Rxck0W_EdvT4LpJB_6Y44fWYXxjdbBgZkiHSp0zB5ER9PPegOtdM1I8p4wBdRrXcqmS6HguJR6z4DmugXZv4SLnJISYhYzSco47zQm6--CiDpgVGATZ52gfzhTwIyg8NJW6iaA0IQHprZUbRFz_abXOuaxgi8eoKPQQKG4pqWFIKpY6bv8c4CiT9zZEn08juU5CJjStpw1Yg3cz0D4vCYgZfuccLC9s2SNS4Gaz0Pl1Rr-RPVJs6hf1uqLku2SFoNtiYaxFbkIdPuT7o5JbLMU6qxzOHh-B4OruJoO7897n6QgRE7mhEkLY0'
-};
-
 //  API variables
 console.log('running');
 var API_URL = `https://apis.accela.com/v4/inspections?module=Building`;
+var accelaOptions = {
+    host: 'apis.accela.com',
+    port: 443,
+    path: '/v4/inspections?module=Building',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'x-accela-appid': '636603438780966771',
+        'Authorization': 'fbD2fS4UE1u2xUc9CjTNRnGthIk6SVvBvJxD7SF8XzgtYRTgzOD3EkLA3ovmFarM9zTXVpX4oalW1L3q6jsE_bTQLNSm5Oa297_seZeFbQnOdVd2sZTLkuIMAfTdQtnStKGtACPJ0Rxck0W_EdvT4LpJB_6Y44fWYXxjdbBgZkiHSp0zB5ER9PPegOtdM1I8p4wBdRrXcqmS6HguJR6z4DmugXZv4SLnJISYhYzSco47zQm6--CiDpgVGATZ52gfzhTwIyg8NJW6iaA0IQHprZUbRFz_abXOuaxgi8eoKPQQKG4pqWFIKpY6bv8c4CiT9zZEn08juU5CJjStpw1Yg3cz0D4vCYgZfuccLC9s2SNS4Gaz0Pl1Rr-RPVJs6hf1uqLku2SFoNtiYaxFbkIdPuT7o5JbLMU6qxzOHh-B4OruJoO7897n6QgRE7mhEkLY0'
+    }
+};
 var request = {};
 
 //  Inspection detail variables
@@ -37,7 +36,7 @@ const today = new Date();
 const yyyymmdd = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2) + "-" + ("0" + today.getDate()).slice(-2);
 
 //  Google Maps variables
-var mapsLink = `https://www.google.com/maps/dir/?api=1&origin=90+Mitchell+St+SW+Atlanta+GA&destination=98+Mitchell+St+SW+Atlanta+GA`;
+const mapsLink = `https://www.google.com/maps/dir/?api=1&origin=90+Mitchell+St+SW+Atlanta+GA&destination=98+Mitchell+St+SW+Atlanta+GA`;
 var mapsReadyAddresses = '';
 
 //  Set inspection search parameters
@@ -61,18 +60,20 @@ function searchStart() {
 
 //  MAIN FUNCTION : Create variables from form input, search API, display results, and display route
 function formSubmitted() {
+    console.log('formSubmitted');
     var searchTerm = "&inspectorIds=" + inspectorId;
         startDate = "&scheduledDateFrom=" + startDate;
         endDate = "&scheduledDateTo=" + endDate;
     
     searchStart();
     search(searchTerm, startDate, endDate)
-    .then(displayResults)
-    .then(calcRoute);
+//    .then(displayResults)
+//    .then(calcRoute);
 }
 
 //  Filter returned Accela API Array to only Scheduled or Rescheduled statuses
 function filterResults(resultAll) {
+    console.log('filterResults');
     var scheduled = ["Scheduled"];
     var filteredArray = resultAll.result.filter(function(itm) {
         return scheduled.indexOf(itm.status.value) > -1;
@@ -82,20 +83,26 @@ function filterResults(resultAll) {
 
 //  Call Accela API
 function search(searchTerm, startDate, endDate) {
+    console.log('search');
     API_URL = `${API_URL}${searchTerm}${startDate}${endDate}`;
-    request = https.request(accelaOptions, {
-        headers: accelaHeaders
-    });
-    return fetch(request)
-    .then(response => response.json())
-    .then(resultAll => filterResults(resultAll))
-    .then(result => {
-        return result.result;
-    });
+    https.get(accelaOptions, res => {
+        console.log('Connected to Accela API');
+        res.on('data', data => {
+            console.log(data);
+        });
+        res.body = JSON.parse(res);
+    })
+    //return fetch(request)
+    //.then(response => response.json())
+    //.then(resultAll => filterResults(resultAll))
+    //.then(result => {
+    //    return result.result;
+    //});
 }
 
 //  Push Accela API results to variable arrays
 function displayResults(returnResult) {
+    console.log('displayResults');
     returnResult.forEach(returnResultEach => {
         recordId.push(returnResultEach.recordId.customId);
         inspectionType.push(returnResultEach.type.value);
@@ -112,6 +119,7 @@ return mapsReadyAddresses;
 
 //  Call Google Maps API
 function calcRoute(mapsReadyAddresses) {
+    console.log('calcRoute');
     var addresses = mapsReadyAddresses;
     var waypoints = '';
     for (var i = 0; i < addresses.length; i++) {
